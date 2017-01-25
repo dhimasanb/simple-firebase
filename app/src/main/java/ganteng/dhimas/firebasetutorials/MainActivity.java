ff14927c8e358a0c1b4482e3cef0b936955f8f15
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-25 22:07:20 +0700

    deklarasi button

--- app/src/main/java/ganteng/dhimas/firebasetutorials/MainActivity.java
+++ app/src/main/java/ganteng/dhimas/firebasetutorials/MainActivity.java
@@ -2,12 +2,17 @@ package ganteng.dhimas.firebasetutorials;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFirebaseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseBtn = (Button) findViewById(R.id.firebase_btn)
     }
 }
