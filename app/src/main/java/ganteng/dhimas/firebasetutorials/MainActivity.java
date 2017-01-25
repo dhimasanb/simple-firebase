34a4ce15c70c466efd2527580288d65a1bb7c317
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-01-25 22:14:01 +0700

    integrasi dengan firebase

--- app/src/main/java/ganteng/dhimas/firebasetutorials/MainActivity.java
+++ app/src/main/java/ganteng/dhimas/firebasetutorials/MainActivity.java
@@ -1,18 +1,33 @@
 package ganteng.dhimas.firebasetutorials;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mFirebaseBtn;
+
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseBtn = (Button) findViewById(R.id.firebase_btn)
+        mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mFirebaseBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                
+            }
+        });
     }
 }
